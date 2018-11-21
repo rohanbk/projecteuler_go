6b84b90702c82ea5ad85e5e2472cb8f59016cb1d
Rohan Balakrishnan <rohanbk@gmail.com>
2018-11-21 14:10:33 -0800

    Solved Problem 10 and 19 and pushed solutions

---
+++ problem10/sieve.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	sieveOfEratothenes(2000000)
+}
+
+func sieveOfEratothenes(n int) {
+	prime := make([]bool, n)
+	for i, _ := range prime {
+		prime[i] = true
+	}
+
+	p := 2
+
+	for p*p <= n {
+		// If prime[i] is not changed, then it is a prime
+		if prime[p] {
+			//Update all multiples of p
+			for j := p * 2; j < n; j += p {
+				if j%p == 0 {
+					prime[j] = false
+				}
+			}
+		}
+		p++
+	}
+	//Iterate over prime slice and if the value is true, then print the index
+	var sum int
+	for i := 2; i < len(prime); i++ {
+		if prime[i] {
+			sum += i
+		}
+	}
+	fmt.Printf("The sume of prime numbers less than %d is %d\n", n, sum)
+}
