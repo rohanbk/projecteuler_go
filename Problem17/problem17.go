e53e0d1ca2d1282c8402da3cf55cef816be0ebae
Rohan Balakrishnan <rohanbk@gmail.com>
2018-11-27 21:51:58 -0800

    Commit solution for project euler problem 17

---
+++ Problem17/problem17.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+)
+
+var units = [10]string{"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var teens = [10]string{"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"}
+var tens = [10]string{"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"}
+
+//HUNDRED string for the word hundred used for numbers > 100
+const HUNDRED = "hundred"
+
+//AND string for 'and' that acts as a delimiter for numbers > 100
+const AND = "and"
+
+var lengthOfNumberStrings map[int]int
+
+func main() {
+	processNumbers(1000)
+}
+
+func processNumbers(upperbound int) {
+	lengthOfNumberStrings = make(map[int]int)
+	sum := 0
+	for i := 1; i <= upperbound; i++ {
+
+		// initialize maps with lengths of corresponding strings
+		if i < 10 {
+			sum += processNumbersLessTen(i)
+		} else if i >= 10 && i < 20 {
+			sum += processNumbersInTeens(i)
+		} else if i >= 20 {
+			sum += processNumbersLargerThanTwenty(i)
+		}
+	}
+	fmt.Println("The total sum is ", sum)
+}
+
+func processNumbersLessTen(n int) int {
+	return len(units[n])
+}
+
+func processNumbersInTeens(n int) int {
+	return len(teens[n%10])
+}
+
+func processNumbersLargerThanTwenty(num int) int {
+	sum := 0
+	i := 0
+	n := num
+	if n > 100 && n%100 != 0 {
+		sum += len(AND)
+	}
+
+	for n > 0 {
+		switch i {
+		case 0:
+			x := units[n%10]
+			sum += len(x)
+		case 1:
+			x := tens[n%10]
+			sum += len(x)
+		case 2:
+			x := units[n%10]
+			sum += len(x)
+			if num < 1000 {
+				sum += len(HUNDRED)
+			}
+		case 3:
+			x := units[n%10]
+			sum += len(x) + len("thousand")
+		}
+		i++
+		n = n / 10
+	}
+
+	return sum
+}
---
+++ Problem17/problem17_test.go
@@ -0,0 +1,113 @@
+package main
+
+import "testing"
+
+func TestProcessNumbersLessTen(t *testing.T) {
+	actualResult := processNumbersLessTen(5)
+	expectedResult := 4 //the word 'five'(5) is 4 characters long
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	actualResult = processNumbersLessTen(8)
+	expectedResult = 5 //the word 'eight'(8) is 5 characters long
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+}
+
+func TestProcessNumbersInTeens(t *testing.T) {
+
+	//the word 'eleven'(10) is 3 characters long
+	actualResult := processNumbersInTeens(10)
+	expectedResult := 3
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'twelve'(12) is 6 characters long
+	actualResult = processNumbersInTeens(12)
+	expectedResult = 6
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'sixteen'(16) is 7 characters long
+	actualResult = processNumbersInTeens(16)
+	expectedResult = 7
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'nineteen'(19) is 8 characters long
+	actualResult = processNumbersInTeens(19)
+	expectedResult = 8
+
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+}
+
+func TestProcessNumbersLargerThanTwenty(t *testing.T) {
+
+	//the word 'twenty one'(21) is 9 characters long
+	actualResult := processNumbersLargerThanTwenty(21)
+	expectedResult := 9
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'sixty nine'(69) is 9 characters long
+	actualResult = processNumbersLargerThanTwenty(69)
+	expectedResult = 9
+	if actualResult != expectedResult {
+		t.Fatalf("Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'one hundred'(100) is 10 characters long
+	actualResult = processNumbersLargerThanTwenty(100)
+	expectedResult = 10
+	if actualResult != expectedResult {
+		t.Fatalf("100: Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'onehundredandone'(101) is 16 characters long
+	actualResult = processNumbersLargerThanTwenty(101)
+	expectedResult = 16
+	if actualResult != expectedResult {
+		t.Fatalf("101: Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'threehundredandfortytwo'(342) is 23 characters long
+	actualResult = processNumbersLargerThanTwenty(342)
+	expectedResult = 23
+	if actualResult != expectedResult {
+		t.Fatalf("342: Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'ninehundredandninetynine'(999) is 24 characters long
+	actualResult = processNumbersLargerThanTwenty(999)
+	expectedResult = 24
+	if actualResult != expectedResult {
+		t.Fatalf("999: Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'onethousand'(1000) is 11 characters long
+	actualResult = processNumbersLargerThanTwenty(1000)
+	expectedResult = 11
+	if actualResult != expectedResult {
+		t.Fatalf("1000: Expected %d but got %d", expectedResult, actualResult)
+	}
+
+	//the word 'onehundredfifteen'(115) is 20 characters long
+	actualResult = processNumbersLargerThanTwenty(115)
+	expectedResult = 20
+	if actualResult != expectedResult {
+		t.Fatalf("115: Expected %d but got %d", expectedResult, actualResult)
+	}
+}
